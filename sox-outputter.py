4ea479d892793c4a9d859689a5aa4430cf890689
Adam Selker <adamselker@gmail.com>
2018-11-30 19:04:15 -0500

    Add Python script to make example Sox .dat file

---
+++ sox-outputter.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+import math
+
+print("; Sample Rate 16000")
+print("; Channels 1")
+
+for i in range(0,100000):
+  print(str(i / 16000) + " " + str(math.sin((i / 16000) * 440 * (2 * math.pi))))
