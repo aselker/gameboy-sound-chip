bb57a83c2c8f74b319fb283be504d253b65d0151
Adam Selker <adamselker@gmail.com>
2018-11-30 19:55:54 -0500

    sox does not seem to care about timestamps?

--- sox-outputter.py
+++ sox-outputter.py
@@ -5,4 +5,5 @@ print("; Sample Rate 16000")
 print("; Channels 1")
 
 for i in range(0,100000):
-  print(str(i / 16000) + " " + str(math.sin((i / 16000) * 440 * (2 * math.pi))))
+  # print(str(i / 16000) + " " + str(math.sin((i / 16000) * 440 * (2 * math.pi))))
+  print("0 " + str(math.sin((i / 16000) * 440 * (2 * math.pi))))
