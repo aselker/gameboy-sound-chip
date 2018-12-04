e661f84ba8cdab30277ff99d4c8834b4a886397b
Adam Selker <adamselker@gmail.com>
2018-12-04 11:13:08 -0500

    Add volume to noise channel, though it does not work

--- noiseChannel.t.v
+++ noiseChannel.t.v
@@ -4,23 +4,24 @@
 module testNoiseChannel();
 	
 	wire clk; baseClk baseclk(clk); // Generate 4mhz signal
+	wire clk256; fixedTimer #(16384) tmr256(clk, clk256);
+	wire clk64; fixedTimer #(4) tmr64(clk256, clk64);
 
 	wire [3:0] left, right;
 	swDac dac(left, right);
 
-	wire lenClk; fixedTimer #(16384) lenTimer(clk, lenClk);
 
 	reg trigger;
-	wire noise;
-	noiseChannel noisechannel(clk, lenClk, 6'd63, 4'd0, 1'd0, 3'd0, 4'd0, 1'd0, 3'd0, trigger, 1'd0, noise);
+	wire [3:0] noise;
+	noiseChannel noisechannel(clk, clk256, clk64, 6'd63, 4'd0, 1'd0, 3'd0, 4'd0, 1'd0, 3'd0, trigger, 1'd0, noise);
 
-	assign left = noise ? 4'd15 : 4'd0;
-	assign right = left;
+	assign left = noise;
+	assign right = noise;
 
 	initial begin
 		trigger = 0; #10; trigger = 1;
 
-		#4194304 $finish;
+		#41943 $finish;
 	end
 
 endmodule
